e6dc25c6c76dae19d5d8fde7315e3d4f6f9d6d14
Kolosek <office@kolosek.com>
2016-10-26 11:43:10 +0200

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020143022) do
+ActiveRecord::Schema.define(version: 20161025101445) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer  "addressable_id",   limit: 10
+    t.string   "addressable_type", limit: 255
+    t.string   "street_name",      limit: 255
+    t.string   "street_number",    limit: 255
+    t.string   "city",             limit: 255
+    t.string   "country",          limit: 255
+    t.datetime "created_at",       limit: 29,  null: false
+    t.datetime "updated_at",       limit: 29,  null: false
+  end
 
   create_table "companies", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at", limit: 29,  null: false
-    t.datetime "updated_at", limit: 29,  null: false
-    t.string   "name",       limit: 255
-    t.datetime "created_at", limit: 29,  null: false
-    t.datetime "updated_at", limit: 29,  null: false
-    t.string   "name",       limit: 255
-    t.datetime "created_at", limit: 29
-    t.datetime "updated_at", limit: 29
+    t.string   "name",        limit: 255
+    t.datetime "created_at",  limit: 29,                 null: false
+    t.datetime "updated_at",  limit: 29,                 null: false
+    t.string   "description", limit: 255, default: "''"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -31,24 +37,34 @@ ActiveRecord::Schema.define(version: 20161020143022) do
     t.integer  "views",        limit: 10,  default: 0
     t.datetime "created_at",   limit: 29,              null: false
     t.datetime "updated_at",   limit: 29,              null: false
-    t.integer  "user_id",      limit: 10
-    t.string   "profile_name", limit: 255
-    t.integer  "views",        limit: 10,  default: 0
-    t.datetime "created_at",   limit: 29,              null: false
-    t.datetime "updated_at",   limit: 29,              null: false
   end
+
+  create_table "profiles_tags", id: false, force: :cascade do |t|
+    t.integer "profile_id", limit: 10, null: false
+    t.integer "tag_id",     limit: 10, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at", limit: 29,  null: false
+    t.datetime "updated_at", limit: 29,  null: false
+  end
+
+  create_table "tags_users", id: false, force: :cascade do |t|
+    t.integer "user_id", limit: 10, null: false
+    t.integer "tag_id",  limit: 10, null: false
+  end
+
+  add_index "tags_users", ["tag_id", "user_id"], name: "index_tags_users_on_tag_id_and_user_id"
+  add_index "tags_users", ["user_id", "tag_id"], name: "index_tags_users_on_user_id_and_tag_id"
 
   create_table "users", force: :cascade do |t|
     t.integer  "company_id", limit: 10
     t.string   "name",       limit: 255
     t.string   "email",      limit: 255
-    t.datetime "created_at", limit: 29,  null: false
-    t.datetime "updated_at", limit: 29,  null: false
-    t.integer  "company_id", limit: 10
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at", limit: 29,  null: false
-    t.datetime "updated_at", limit: 29,  null: false
+    t.datetime "created_at", limit: 29,              null: false
+    t.datetime "updated_at", limit: 29,              null: false
+    t.integer  "credit",     limit: 10,  default: 0
   end
 
 end
