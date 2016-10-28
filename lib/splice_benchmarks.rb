72696c76cccbd0b73bdd503263f66a28ff6e8321
Kolosek <office@kolosek.com>
2016-10-28 15:14:21 +0200

    Updated benchmark

--- lib/splice_benchmarks.rb
+++ lib/splice_benchmarks.rb
@@ -1,4 +1,5 @@
 require 'benchmark'
+require 'database_cleaner'
 
 module SpliceBenchmarks
   include Benchmark
@@ -11,33 +12,41 @@ module SpliceBenchmarks
       x.report('create: ') { RUN_CIRCLE.times {|i| Company.create!(name: "Company#{i}")} }
       DatabaseCleaner.clean
 
-      company = Company.create!(id: nil, name: 'Company');
+      company = Company.create!(name: 'Company');
       x.report('update: ') { RUN_CIRCLE.times {|i| company.update(name: "Was Updated #{i}") } }
       DatabaseCleaner.clean
 
-      company = Company.create!(id: nil, name: 'Company');
-      x.report('find: ') { RUN_CIRCLE.times {|i| Company.find(company.id) } }
+      # Not supported
+      # company = Company.create!(name: 'Company');
+      # x.report('find: ') { RUN_CIRCLE.times {|i| Company.find(company.id) } }
+      # DatabaseCleaner.clean
+
+      company = Company.create!(name: 'Company');
+      x.report('where: ') { RUN_CIRCLE.times {|i| Company.where(name: 'Company').to_a } }
       DatabaseCleaner.clean
 
-      company = Company.create!(id: nil, name: 'Company');
-      x.report('where: ') { RUN_CIRCLE.times {|i| Company.where(name: 'Company').first } }
-      DatabaseCleaner.clean
-
-      company = Company.create!(id: nil, name: 'Company');
+      company = Company.create!(name: 'Company');
       3.times { User.create!(company: company) }
-      company2 = Company.create!(id: nil, name: 'Company2');
+      company2 = Company.create!(name: 'Company2');
       3.times { User.create!(company: company2) }
       x.report('join: ') { RUN_CIRCLE.times {|i| User.joins(:company).where(companies: {name: 'Company'}).to_a } }
       DatabaseCleaner.clean
 
-      company = Company.create!(id: nil, name: 'Company');
+      company = Company.create!(name: 'Company');
       6.times { User.create!(company: company) }
-      x.report('includes: ') { RUN_CIRCLE.times {|i| Company.includes(:users).find(company.id) } }
+      x.report('includes: ') { RUN_CIRCLE.times {|i| Company.includes(:users).to_a } }
       DatabaseCleaner.clean
 
-      company = Company.create!(id: nil, name: 'Company1');
+      company = Company.create!(name: 'Company1');
       4.times { User.create!(company: company, credit: 10) }
-      company2 = Company.create!(id: nil, name: 'Company2');
+      company2 = Company.create!(name: 'Company2');
+      3.times { User.create!(company: company2, credit: 10) }
+      x.report('group: ') { RUN_CIRCLE.times {|i| User.group('company_id').count } }
+      DatabaseCleaner.clean
+
+      company = Company.create!(name: 'Company1');
+      4.times { User.create!(company: company, credit: 10) }
+      company2 = Company.create!(name: 'Company2');
       3.times { User.create!(company: company2, credit: 10) }
       x.report('group+having: ') { RUN_CIRCLE.times {|i| User.having('SUM(credit) > 30').group('company_id').count } }
       DatabaseCleaner.clean
@@ -46,9 +55,10 @@ module SpliceBenchmarks
       x.report('order: ') { RUN_CIRCLE.times {|i| Company.order(:name).to_a } }
       DatabaseCleaner.clean
 
-      4.times {|i| Company.create!(name: "Company #{i}") }
-      x.report('limit: ') { RUN_CIRCLE.times {|i| Company.limit(2).to_a } }
-      DatabaseCleaner.clean
+      # Not supported
+      # 4.times {|i| Company.create!(name: "Company #{i}") }
+      # x.report('limit: ') { RUN_CIRCLE.times {|i| Company.limit(2).to_a } }
+      # DatabaseCleaner.clean
 
       4.times {|i| Company.create!(name: "Company #{i}") }
       x.report('select: ') { RUN_CIRCLE.times {|i| Company.select(:name, :created_at).to_a } }
@@ -60,9 +70,10 @@ module SpliceBenchmarks
       x.report('distinct: ') { RUN_CIRCLE.times {|i| Company.select(:name).distinct.to_a } }
       DatabaseCleaner.clean
 
-      8.times {|i| Company.create!(name: "Company #{i}") }
-      x.report('offset: ') { RUN_CIRCLE.times {|i| Company.offset(4).to_a } }
-      DatabaseCleaner.clean
+      # Not supported
+      # 8.times {|i| Company.create!(name: "Company #{i}") }
+      # x.report('offset: ') { RUN_CIRCLE.times {|i| Company.offset(4).to_a } }
+      # DatabaseCleaner.clean
     end
     DatabaseCleaner.clean
   end
