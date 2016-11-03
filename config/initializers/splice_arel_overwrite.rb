466deeaa9cbb89353aa40c537958ba714c149b9b
Kolosek <office@kolosek.com>
2016-11-03 17:22:33 +0100

    Fixed issue with database limit and offset issue cause in Rails Arel classes

---
+++ config/initializers/splice_arel_overwrite.rb
@@ -0,0 +1,13 @@
+Arel::Visitors::ToSql.class_eval do
+  def visit_Arel_Nodes_Offset o, collector
+    collector << "OFFSET "
+    visit o.expr, collector
+    collector << " ROWS"
+  end
+
+  def visit_Arel_Nodes_Limit o, collector
+    collector << "FETCH FIRST "
+    visit o.expr, collector
+    collector << " ROWS ONLY"
+  end
+end
