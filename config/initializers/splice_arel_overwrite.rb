c9eff6ad7fa6342978839388e587746ded363d9a
Kolosek <office@kolosek.com>
2016-11-08 14:46:30 +0100

    Added ruby version checker for overwriting methods

--- config/initializers/splice_arel_overwrite.rb
+++ config/initializers/splice_arel_overwrite.rb
@@ -1,13 +1,15 @@
-Arel::Visitors::ToSql.class_eval do
-  def visit_Arel_Nodes_Offset o, collector
-    collector << "OFFSET "
-    visit o.expr, collector
-    collector << " ROWS"
-  end
+if  RbConfig::CONFIG['ruby_install_name'] == 'jruby'
+  Arel::Visitors::ToSql.class_eval do
+    def visit_Arel_Nodes_Offset o, collector
+      collector << "OFFSET "
+      visit o.expr, collector
+      collector << " ROWS"
+    end
 
-  def visit_Arel_Nodes_Limit o, collector
-    collector << "FETCH FIRST "
-    visit o.expr, collector
-    collector << " ROWS ONLY"
+    def visit_Arel_Nodes_Limit o, collector
+      collector << "FETCH FIRST "
+      visit o.expr, collector
+      collector << " ROWS ONLY"
+    end
   end
 end
--- config/initializers/splice_last_id_overwrite.rb
+++ config/initializers/splice_last_id_overwrite.rb
@@ -1,40 +1,42 @@
-ActiveRecord::ConnectionAdapters::JdbcAdapter.class_eval do
-    def exec_insert(sql, name, binds, pk = nil, sequence_name = nil)
-      if sql.respond_to?(:to_sql)
-        sql = to_sql(sql, binds); to_sql = true
-      end
-
-      val = nil
-      if prepared_statements?
-        val = log(sql, name || 'SQL', binds) { @connection.execute_insert(sql, binds) }
-      else
-        sql = suble_binds(sql, binds) unless to_sql # deprecated behavior
-        val = log(sql, name || 'SQL') { @connection.execute_insert(sql) }
-      end
-
-      if val.nil? && pk
-        unless sequence_name
-          table_ref = extract_table_ref_from_insert_sql(sql)
-          sequence_name = default_sequence_name(table_ref, pk)
-          return val unless sequence_name
+if  RbConfig::CONFIG['ruby_install_name'] == 'jruby'
+  ActiveRecord::ConnectionAdapters::JdbcAdapter.class_eval do
+      def exec_insert(sql, name, binds, pk = nil, sequence_name = nil)
+        if sql.respond_to?(:to_sql)
+          sql = to_sql(sql, binds); to_sql = true
         end
-        last_insert_id_result(sequence_name)
-      else
-        val
+
+        val = nil
+        if prepared_statements?
+          val = log(sql, name || 'SQL', binds) { @connection.execute_insert(sql, binds) }
+        else
+          sql = suble_binds(sql, binds) unless to_sql # deprecated behavior
+          val = log(sql, name || 'SQL') { @connection.execute_insert(sql) }
+        end
+
+        if val.nil? && pk
+          unless sequence_name
+            table_ref = extract_table_ref_from_insert_sql(sql)
+            sequence_name = default_sequence_name(table_ref, pk)
+            return val unless sequence_name
+          end
+          last_insert_id_result(sequence_name)
+        else
+          val
+        end
       end
-    end
 
-    private
-    def extract_table_ref_from_insert_sql(sql) # :nodoc:
-      sql[/into\s+([^\(]*).*values\s*\(/im]
-      $1.strip if $1
-    end
+      private
+      def extract_table_ref_from_insert_sql(sql) # :nodoc:
+        sql[/into\s+([^\(]*).*values\s*\(/im]
+        $1.strip if $1
+      end
 
-    def last_insert_id_result(sequence_name) #:nodoc:
-      exec_query("SELECT IDENTITY_VAL_LOCAL() FROM #{sequence_name}", 'SQL')
-    end
+      def last_insert_id_result(sequence_name) #:nodoc:
+        exec_query("SELECT IDENTITY_VAL_LOCAL() FROM #{sequence_name}", 'SQL')
+      end
 
-    def default_sequence_name(table, column)
-      "#{table}"
-    end
+      def default_sequence_name(table, column)
+        "#{table}"
+      end
+  end
 end
