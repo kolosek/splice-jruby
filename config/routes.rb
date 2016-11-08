694f1a848510364eaec3623724fbfab5532946fb
Kolosek <office@kolosek.com>
2016-11-08 14:45:13 +0100

    Updated routes for the new benchmmark requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root :to => "static#index"
+
+  resources :benchmarks do
+    collection do
+      get :method_create
+      get :method_update
+      get :method_where
+    end
+  end
 end
