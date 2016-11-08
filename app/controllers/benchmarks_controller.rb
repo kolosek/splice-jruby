c6cb1a1266b5da43e9437573fdf7791f73a7c9e4
Kolosek <office@kolosek.com>
2016-11-08 14:44:57 +0100

    Added new benchmark controller for testing server response

---
+++ app/controllers/benchmarks_controller.rb
@@ -0,0 +1,21 @@
+class BenchmarksController < ApplicationController
+
+  def method_create
+    Company.create!(name: "Company")
+    head :ok, content_type: "text/html"
+  end
+
+  def method_update
+    company = Company.first
+    company.update!(name: "New Company Name")
+
+    head :ok, content_type: "text/html"
+  end
+
+  def method_where
+    companies = Company.where(id: 551)
+    ids = companies.ids
+    head :ok, content_type: "text/html"
+  end
+
+end
