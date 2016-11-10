365082916644c7e7b49b8f2f249f81c06d18dc8f
Kolosek <office@kolosek.com>
2016-11-10 13:53:19 +0100

    Added additional benchmarks to the controller

--- app/controllers/benchmarks_controller.rb
+++ app/controllers/benchmarks_controller.rb
@@ -13,13 +13,32 @@ class BenchmarksController < ApplicationController
   end
 
   def method_where
-    companies = Company.where(id: 551)
+    # Create a company in console before testing it out
+    companies = Company.where(id: 1)
     ids = companies.ids
     head :ok, content_type: "text/html"
   end
 
   def method_limit
     companies = Company.limit(1)
+    ids = companies.ids
+    head :ok, content_type: "text/html"
+  end
+
+  def method_offset
+    companies = Company.offset(1)
+    ids = companies.ids
+    head :ok, content_type: "text/html"
+  end
+
+  def method_group
+    companies = Company.group(:name)
+    ids = companies.ids
+    head :ok, content_type: "text/html"
+  end
+
+    def method_select
+    companies = Company.select :id
     ids = companies.ids
     head :ok, content_type: "text/html"
   end
